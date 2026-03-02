d93e35b490e8a1006e61bcd1330110877dd9fdd9
NilsPetsch1999 <83291289+NilsPetsch1999@users.noreply.github.com>
2026-03-02 02:58:52 +0100

    spring boot project setup

---
+++ tdms/src/main/java/fhcampus/nilspetsch/tdms/TdmsApplication.java
@@ -0,0 +1,13 @@
+package fhcampus.nilspetsch.tdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdmsApplication.class, args);
+	}
+
+}
